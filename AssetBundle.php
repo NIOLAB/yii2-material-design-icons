b105408b75c86e81c7f91f9d4eefd0faef71cdbe
Harry Doddema <harry@niomail.nl>
2020-02-26 11:31:56 +0100

    test with cdn url

--- AssetBundle.php
+++ AssetBundle.php
@@ -8,28 +8,28 @@ namespace yii\materialicons;
  */
 class AssetBundle extends \yii\web\AssetBundle
 {
-    /**
-     * @inheritdoc
-     */
-    public $sourcePath = '@vendor/mervick/material-design-icons';
+//    /**
+//     * @inheritdoc
+//     */
+//    public $sourcePath = '@vendor/mervick/material-design-icons';
 
     /**
      * @inheritdoc
      */
     public $css = [
-        'css/material-icons.min.css',
+        'https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css',
     ];
 
-    /**
-     * Initializes the bundle.
-     * Set publish options to copy only necessary files (in this case css and font folders)
-     */
-    public function init()
-    {
-        parent::init();
-
-        $this->publishOptions['beforeCopy'] = function ($from, $to) {
-            return preg_match('%(/|\\\\)(fonts|css)%', $from);
-        };
-    }
+//    /**
+//     * Initializes the bundle.
+//     * Set publish options to copy only necessary files (in this case css and font folders)
+//     */
+//    public function init()
+//    {
+//        parent::init();
+//
+//        $this->publishOptions['beforeCopy'] = function ($from, $to) {
+//            return preg_match('%(/|\\\\)(fonts|css)%', $from);
+//        };
+//    }
 }
